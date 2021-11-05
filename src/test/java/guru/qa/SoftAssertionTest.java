21357de1a017795b00070acdc6b8463fe285747d
juliazvereva <j.zvereva050@gmail.com>
2021-11-05 21:52:42 +0300

    tasks completed

--- src/test/java/guru/qa/SoftAssertionTest.java
+++ src/test/java/guru/qa/SoftAssertionTest.java
@@ -1,18 +1,27 @@
 package guru.qa;
 
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SoftAssertionTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
 
     @Test
     void softAssertionPageIsPresent() {
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").$(byText("Wiki")).click();
-        $(".wiki-more-pages-link").click();
-        $("#wiki-pages-box").$(byTagName("a")).shouldHave(Condition.text("SoftAssertions"));
-
+        $(".wiki-more-pages-link > button").click();
+        $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
+        $(".wiki-rightbar").$(byText("SoftAssertions")).click();
+        $(".Layout-main").shouldHave(text("Using JUnit5 extend test class:"));
     }
 }
